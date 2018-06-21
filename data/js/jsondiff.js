ddd0d6c314068ac88b8db71494392919f68ac767
rafspiny <rafspiny@gmail.com>
2018-06-21 22:31:38 +0200

    Simplified code for computing JSON difference

--- data/js/jsondiff.js
+++ data/js/jsondiff.js
@@ -1,20 +1,9 @@
 /**
  * jsondiff namespace.
  */
-// var EXPORTED_SYMBOLS = ["jsonDiff"];
-// function jsondiff() {
-  	
-//   };
 if (typeof jsondiff == "undefined") {
   var jsondiff = {
     isArray: function(value) {
-    	// console.log("TYPEOF: ");
-	    // console.log(typeof value);
-	    // console.log("CONSTRUCTOR: ");
-	    // console.log(value.constructor);
-	    // console.log(value.constructor === Array);
-    	// console.log("INSIDE: ");
-    	// console.log(value && typeof value === "object" && value.constructor === Array);
     	return value && typeof value === "object" && value.constructor === Array;
     },
     typeOf: function(value) {
@@ -55,6 +44,7 @@ if (typeof jsondiff == "undefined") {
 		return diff;
 	},
 	populateDiff: function(origin, copy, diff) {
+	    // This method populate the diff object with all the differences between the two JSON objects
 		// What are we?
 		var typeOrigin = this.typeOf(origin);
 	    var typeCopy   = this.typeOf(copy);
@@ -70,66 +60,74 @@ if (typeof jsondiff == "undefined") {
 			}
 	    }
 		if (typeOrigin == 'object') {
-			for (var key_origin in origin) {
-				diff.values[key_origin] = this.getFilledTemplateDiffObject({"key":key_origin, "value":origin[key_origin], "status":"untouched"});
-
-				if (!copy.hasOwnProperty(key_origin)) {
-					// The key has been removed
-					diff.values[key_origin].status = "removed";
-					this.recursivelyFillDiffObj(origin[key_origin], diff.values[key_origin]);
-				} else {
-					// The key is there, let's check if it is changed
-					this.populateDiff(origin[key_origin], copy[key_origin], diff.values[key_origin]);
-				}
-			}
-			for (var key_copy in copy) {
-				diff.values[key_copy] = this.getFilledTemplateDiffObject({"key":key_copy, "value":copy[key_copy], "status":"untouched"});
-
-				if (!origin.hasOwnProperty(key_copy)) {
-					// The key has been added
-					diff.values[key_copy].status = "added";
-					this.recursivelyFillDiffObj(copy[key_copy], diff.values[key_copy]);
-				} else {
-					// The key is there, let's check if it is changed 
-					this.populateDiff(origin[key_copy], copy[key_copy], diff.values[key_copy]);
-				}
-			}
-		} else {
-			if (typeOrigin == 'array') {
-				var minLength = Math.min(origin.length, copy.length);
-				for (var common_index = 0; common_index < minLength; common_index++) {
-					var common_object = origin[common_index];
-					var valueOfCopy = copy[common_index];
-					diff.values[common_index] = this.getFilledTemplateDiffObject({"key":common_index, "value":common_object, "status":"untouched"});
-					this.populateDiff(common_object, valueOfCopy, diff.values[common_index]);
-				}
-
-				for (var source_index = minLength; source_index < origin.length; source_index++) {
-					var source_object = origin[source_index];
-					diff.values[source_index] = this.getFilledTemplateDiffObject({"key":source_index, "value":source_object, "status":"untouched"});
-					diff.values[source_index].status = "removed";
-					this.recursivelyFillDiffObj(origin[source_index], diff.values[source_index]);
-				}
-				for (var destination_index = minLength; destination_index < copy.length; destination_index++) {
-					var destination_object = copy[destination_index];
-					diff.values[destination_index] = this.getFilledTemplateDiffObject({"key":destination_index, "value":destination_object, "status":"untouched"});
-					diff.values[destination_index].status = "added";
-					this.recursivelyFillDiffObj(copy[destination_index], diff.values[destination_index]);
-				}
-			} else {
-				if (origin === undefined)
-					diff.status = "added";
-				if (copy === undefined)
-					diff.status = "removed";
-
-				diff.type = this.typeOf(origin); 
-				if (origin === copy)
-					diff.status = "untouched";
-				else
-					diff.status = "changed";
-			}
+		    // Recursively populate the diff object if the type of the source object is 'object'
+			this.process_object(origin, copy, diff)
+			return
 		}
-	}
+		if (typeOrigin == 'array') {
+            this.process_array(origin, copy, diff)
+        } else {
+            if (origin === undefined)
+                diff.status = "added";
+            if (copy === undefined)
+                diff.status = "removed";
+
+            diff.type = this.typeOf(origin);
+            if (origin === copy)
+                diff.status = "untouched";
+            else
+                diff.status = "changed";
+        }
+	},
+	process_object: function(origin, copy, diff) {
+	    // Check the added elements and the removed ones
+	    this.handle_object(origin, copy, diff, 'removed', 1)
+	    this.handle_object(origin, copy, diff, 'added', 0)
+	},
+	handle_object: function(origin, copy, diff, label, selector) {
+	    // Inspect each element between two JSON objects
+	    var source = copy;
+	    var destination = origin;
+
+	    if (selector == 1) {
+	        source = origin;
+	        destination = copy;
+	    }
+
+	    for (var key in source) {
+            diff.values[key] = this.getFilledTemplateDiffObject({"key":key, "value":source[key], "status":"untouched"});
+            if (!destination.hasOwnProperty(key)) {
+                // The key has been removed
+                diff.values[key].status = label;
+                this.recursivelyFillDiffObj(source[key], diff.values[key]);
+            } else {
+                // The key is there, let's check if it is changed
+                this.populateDiff(origin[key], copy[key], diff.values[key]);
+            }
+        }
+	},
+	process_array: function(origin, copy, diff) {
+	    var minLength = Math.min(origin.length, copy.length);
+        for (var common_index = 0; common_index < minLength; common_index++) {
+            var common_object = origin[common_index];
+            var valueOfCopy = copy[common_index];
+            diff.values[common_index] = this.getFilledTemplateDiffObject({"key":common_index, "value":common_object, "status":"untouched"});
+            this.populateDiff(common_object, valueOfCopy, diff.values[common_index]);
+        }
+
+        for (var source_index = minLength; source_index < origin.length; source_index++) {
+            var source_object = origin[source_index];
+            diff.values[source_index] = this.getFilledTemplateDiffObject({"key":source_index, "value":source_object, "status":"untouched"});
+            diff.values[source_index].status = "removed";
+            this.recursivelyFillDiffObj(origin[source_index], diff.values[source_index]);
+        }
+        for (var destination_index = minLength; destination_index < copy.length; destination_index++) {
+            var destination_object = copy[destination_index];
+            diff.values[destination_index] = this.getFilledTemplateDiffObject({"key":destination_index, "value":destination_object, "status":"untouched"});
+            diff.values[destination_index].status = "added";
+            this.recursivelyFillDiffObj(copy[destination_index], diff.values[destination_index]);
+        }
+    }
   };
 }
 
